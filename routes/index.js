81458a5822ae763699737e3ec9bedf5d7adfb6a0
ajpuentedura <ajpuentedura@gmail.com>
2015-08-11 21:34:32 +0200

    Error cert

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/quizes/:quizId(\\d+)/edit',  sessionController.loginRequired, quizC
 router.put('/quizes/:quizId(\\d+)',	  sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',	 sessionController.loginRequired,  quizController.destroy);
 
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController,new);
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
