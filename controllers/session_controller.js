e511abcb6436827ffd40b78794ddc072fa30d373
ajpuentedura <ajpuentedura@gmail.com>
2015-08-11 21:38:27 +0200

    Error cert

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,7 +1,7 @@
 //Get /login -- Formulario de login
 exports.new= function(req,res){
 	var errors= req.session.errors || {};
-	req.session.errors={}:
+	req.session.errors={};
 	
 	res.render('sessions/new',{errors:errors});
 };
