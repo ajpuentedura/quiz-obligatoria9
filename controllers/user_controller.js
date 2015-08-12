6cd3eda455901d1018370a3a9b725396318bc15c
ajpuentedura <ajpuentedura@gmail.com>
2015-08-12 19:34:32 +0200

    Problema nombre fichero

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,10 +1,11 @@
 var users= {admin: {id:1, username: "admin", password:"1234"},
 			pepe: {id:2, username:"pepe", password:"5678"}
 			};
-exports.autenticar= function(login, password, callback){
-if(users[login]){
-	if(password=== users[login].password){
-		callback(null,user[login]);
-	}else{callback(new Error('Password erróneo.'));}
-}else {callback (new Error('No existe el usuario.'));}
-};
+	exports.autenticar = function(login, password, callback) {
+		if(users[login]){
+			if(password === users[login].password){
+				callback(null, users[login]);
+			}
+			else { callback(new Error('Password erróneo.')); }
+		} else { callback(new Error('No existe el usuario.'));}
+	};
