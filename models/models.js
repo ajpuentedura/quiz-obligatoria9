4646b75b7f98756076fa583e77500b812c3985e8
ajpuentedura <ajpuentedura@gmail.com>
2015-08-12 20:17:56 +0200

    Problema ficheros

--- models/models.js
+++ models/models.js
@@ -1,33 +1,30 @@
-/* NOTA: models.js construye la DB y el modelo importando (quiz.js)
-sequelize.sync() construye la DB según define el modelo.*/
 var path = require('path');
-//------------------------------------------------------
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite DATABASE_URL = sqlite://:@:/
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6]||null);
-var user = (url[2]||null);
-var pwd = (url[3]||null);
-var protocol = (url[1]||null);
-var dialect = (url[1]||null);
-var port = (url[5]||null);
-var host = (url[4]||null);
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:post/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*?)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
 var storage = process.env.DATABASE_STORAGE;
-//------------------------------------------------------
+
 // Cargar Modelo ORM
-var Sequelize = require('sequelize'); //importamos la librería del ORM
-//------------------------------------------------------
+var Sequelize = require('sequelize');
+
 // Usar BBDD SQLite o Postgres
-var sequelize = new Sequelize(DB_name, user, pwd,
-	{ dialect: protocol,
+var sequelize = new Sequelize( DB_name, user, pwd,
+      { dialect: protocol,
 	protocol: protocol,
 	port: port,
 	host: host,
-	storage: storage, // solo SQLite (.env)
-	omitNull: true // solo Postgres
-	}
+	storage: storage,
+	omitNull: true
+      }
 );
-//------------------------------------------------------
 
 // Importar la definicion de la tabla Quiz en models/quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
--- models/quiz.js
+++ models/quiz.js
@@ -1,22 +1,20 @@
-// Definicion del modelo de Quiz con validación
+// Definicion del modelo Quiz
 
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define(
-    'Quiz',
-    { pregunta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-      },
-      respuesta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-      },
-    
-	 tema:
-                {
-                    type:DataTypes.STRING,
-                    validate:{ notEmpty: {msg: "-> Falta tema"}}
-                }
-}
-  );
-}
+module.exports = function(sequelize, DataTypes){
+   return sequelize.define(
+      'Quiz',
+	{ pregunta: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+            },
+	  respuesta: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+            },
+	  tematica: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Temática"}}
+            },
+        }
+     );
+};
